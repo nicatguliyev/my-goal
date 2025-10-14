e06edbc2152ff4c45af82b7ee202fffbf23bde1e
Nicat Güliyev <ni.guliyev@ady.az>
2025-10-14 15:34:45 +0400

    some changes

--- App.js
+++ App.js
@@ -33,7 +33,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
      flex: 1,
-    //  padding: 16,
+     padding: 16,
      backgroundColor: "white",
   },
 });
