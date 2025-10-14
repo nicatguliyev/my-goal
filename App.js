7db0ca52eb0d0e75ffc94b98ea949a2b498f256a
Nicat Güliyev <ni.guliyev@ady.az>
2025-10-14 15:32:51 +0400

    some changes

--- App.js
+++ App.js
@@ -18,8 +18,8 @@ export default function App() {
     <SafeAreaProvider>
       <SafeAreaView style = {{flex: 1, backgroundColor: "white" }} edges={["top", "bottom"]}>
         <View style={styles.container}>
-          <CategoryListModal />
-          {/* <GoalsScreen /> */}
+          {/* <CategoryListModal /> */}
+          <GoalsScreen />
           {/* <BottomSheet /> */}
           {/* <CategoryListItem  name = "Search" icon = "search" checked={true}/> */}
           <StatusBar style="auto" />
